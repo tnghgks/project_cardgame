67c6e33d8912215d1d8385474a4f5e28157404f0
HOSU HAN <tnghgks@naver.com>
2022-11-24 20:35:49 +0900

    📝 [ 한호수 ] 주석 삭제 ( 임시 코드 주석 삭제)

--- js/components/CardList.js
+++ js/components/CardList.js
@@ -5,6 +5,8 @@ class CardList extends Component {
     super($target);
     this.cardData = cardData;
   }
+  render() {}
+
   template(card) {
     return `
     <li class="item-card">
@@ -18,20 +20,5 @@ class CardList extends Component {
     });
   }
 }
-// cardData.forEach((card) => {
-//   const $li = document.createElement("li");
-//   const $frontDiv = document.createElement("div");
-//   const $backDiv = document.createElement("div");
-
-//   $frontDiv.classList.add("div-front");
-//   $frontDiv.classList.add("div-hidden");
-//   $backDiv.classList.add("div-back");
-//   $li.classList.add("item-card");
-//   $li.appendChild($backDiv);
-//   $li.appendChild($frontDiv);
-//   $frontDiv.style.backgroundPosition = `${card.position.x}px ${card.position.y}px`;
-
-//   $cardList.appendChild($li);
-// });
 
 export default CardList;
