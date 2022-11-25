50c1c6467686fac6cc4c3e777df53c07b98dfd2b
Heesu <heesu0303@naver.com>
2022-11-26 03:24:04 +0900

    💄 [ 장희수] 모달창 스타일 추가

--- js/components/Modal.js
+++ js/components/Modal.js
@@ -1,18 +1,22 @@
 import Component from "../core/Component.js";
 
 class Modal extends Component {
-  constructor(score, winLose, restart, goHome) {
-    super(document.querySelector(".art-modal"));
-    this.score = score;
-    this.winLose = winLose;
-    this.restart = restart;
-    this.goHome = goHome;
-    this.setup();
-  }
+    constructor(score, winLose, restart, goHome) {
+        super(document.querySelector(".art-modal"));
+        this.score = score;
+        this.winLose = winLose;
+        this.restart = restart;
+        this.goHome = goHome;
+        this.setup();
+    }
 
-  template() {
-    return `<div class="modal">안녕하세요 모달입니다.</div>`;
-  }
+    template() {
+        return `
+          <article class="modal">게임 종료</article>
+            
+
+        `;
+    }
 }
 
 export default Modal;
