1d70807106f53c65d90bfef0cf9419c40f4123bf
Heesu <heesu0303@naver.com>
2022-11-30 02:26:32 +0900

    🐛 [ 장희수 ] 버그 수정 (스코어보드 점수 오류 수정, 카드 다 맞췄을 때 모달창 버튼 안먹힘 수정)

--- js/components/CardGame.js
+++ js/components/CardGame.js
@@ -4,38 +4,39 @@ import Modal from "./Modal.js";
 import CardList from "./CardList.js";
 
 class CardGame extends Component {
-  constructor($target) {
-    // 점수 관련된 친구들 객체로 변환
-    super($target);
-    this.clickCount = 0;
-    this.hitScore = 0;
-    this.failScore = 0;
-    this.victory = 0;
-    this.defeat = 0;
-    this.totalFlipCount = 0;
-    this.totalHitScore = 0;
-    this.totalFailScore = 0;
-    this.level = 0;
-    this.$prevCardItem = "";
-    this.prevCardData = "";
-    this.timerId = "";
-    this.getScoreInLocal();
-  }
+    constructor($target) {
+        // 점수 관련된 친구들 객체로 변환
+        super($target);
+        this.clickCount = 0;
+        this.hitScore = 0;
+        this.failScore = 0;
+        this.victory = 0;
+        this.defeat = 0;
+        // this.totalFlipCount = 0;
+        this.totalHitScore = 0;
+        this.totalFailScore = 0;
+        this.level = 0;
+        this.$prevCardItem = "";
+        this.prevCardData = "";
+        this.timerId = "";
+        this.getScoreInLocal();
+    }
 
-  getScoreInLocal() {
-    const scoreData = JSON.parse(localStorage.getItem("scoreBoard"));
+    // 로컬스토리지에 저장한 스코어 정보 가져오기
+    getScoreInLocal() {
+        const scoreData = JSON.parse(localStorage.getItem("scoreBoard"));
 
-    if (!scoreData) return;
-    console.log(scoreData);
-    this.victory = scoreData.victory;
-    this.defeat = scoreData.defeat;
-    this.totalFlipCount = scoreData.totalFlipCount;
-    this.totalHitScore = scoreData.totalHitScore;
-    this.totalFailScore = scoreData.totalHitScore;
-  }
+        if (!scoreData) return;
+        console.log(scoreData);
+        this.victory = scoreData.victory;
+        this.defeat = scoreData.defeat;
+        // this.totalFlipCount = scoreData.totalFlipCount;
+        this.totalHitScore = scoreData.totalHitScore;
+        this.totalFailScore = scoreData.totalHitScore;
+    }
 
-  template() {
-    return ` 
+    template() {
+        return ` 
     <h1 class="tit-game">피카츄 카드 게임</h1>
     <main class="main-wrapper">
         <article class="arti-level-setting">
@@ -49,233 +50,247 @@ class CardGame extends Component {
         <h2 class="tit-score-board ir">스코어 보드</h2>
         <marquee scrolldelay="100" scrollamount="20">
         <strong>종합점수</strong>
-        총 승리 : ${this.victory}
-        총 패배 : ${this.defeat}
+        총 승리:${this.victory}
+        총 패배:${this.defeat} /
         <strong>난이도별 클리어 타임</strong>
-        어려움 (8*8) : 64s
-        중간 (6*6) : 54s
-        쉬움 (4*4) : 32s
+        쉬움(4x4):32s
+        중간(6x6):54s
+        어려움(8x8):64s /
         <strong>이전 기록</strong>
-        뒤집은 총 횟수 : ${this.totalFlipCount}
-        맞은 횟수 : ${this.totalHitScore}
-        틀린 횟수 : ${this.totalFailScore}
+        뒤집은 총 횟수:${this.totalHitScore + this.totalFailScore}
+        맞은 횟수:${this.totalHitScore}
+        틀린 횟수:${this.totalFailScore}
         </marquee>
       </article>
     </main>
     <footer></footer>
     `;
-  }
-
-  handleLevelBtn(event) {
-    if (!(event.target.tagName === "BUTTON")) {
-      return;
     }
 
-    //이동시 배경 요소를 변경하는 옵션
-    this.inGameStyle();
-    this.level = Number(event.target.dataset.level);
-    this.paintCardGame(this.level);
-  }
-  // 난이도 설정 버튼에 이벤트 리스너 추가
-  addEvent() {
-    const $article = document.querySelector(".arti-level-setting");
+    handleLevelBtn(event) {
+        if (!(event.target.tagName === "BUTTON")) {
+            return;
+        }
 
-    $article.addEventListener("click", this.handleLevelBtn.bind(this));
-  }
-  // json파일에서 카드 데이터 불러오기
-  async getData(totalCardCount) {
-    const data = await (await fetch(`json/card.json`)).json();
+        //이동시 배경 요소를 변경하는 옵션
+        this.inGameStyle();
+        this.level = Number(event.target.dataset.level);
+        this.paintCardGame(this.level);
+    }
 
-    return data.slice(0, totalCardCount / 2);
-  }
+    // 난이도 설정 버튼에 이벤트 리스너 추가
+    addEvent() {
+        const $article = document.querySelector(".arti-level-setting");
 
-  //카드 그려주기
-  async paintCardGame(level) {
-    this.level = level;
-    this.$target.innerHTML = "";
-    this.totalCardCount = level * level;
-    this.limitTime = level * 10;
-    // this.limitTime = 2;
+        $article.addEventListener("click", this.handleLevelBtn.bind(this));
+    }
 
-    // 모든 카드 렌더링
-    const cardData = await this.getData(this.totalCardCount);
-    const totalCardData = cardData.concat(cardData);
+    // json파일에서 카드 데이터 불러오기
+    async getData(totalCardCount) {
+        const data = await (await fetch(`json/card.json`)).json();
 
-    // 랜덤하게 모든 카드를 섞기
-    totalCardData.sort(() => Math.random() - 0.5);
+        return data.slice(0, totalCardCount / 2);
+    }
 
     //카드 그려주기
-    this.paintCard(totalCardData);
+    async paintCardGame(level) {
+        this.level = level;
+        this.$target.innerHTML = "";
+        this.totalCardCount = level * level;
+        this.limitTime = level * 10;
+        // this.limitTime = 2;
 
-    //타이머 실행
-    this.timer(this.limitTime);
-  }
+        // 모든 카드 렌더링
+        const cardData = await this.getData(this.totalCardCount);
+        const totalCardData = cardData.concat(cardData);
 
-  //카드 Elements 생성해서 Rendor 하기
-  paintCard(cardData) {
-    const $cardList = document.createElement("ul");
-    $cardList.classList.add("list-card");
-    $cardList.addEventListener("click", this.flipCard.bind(this));
+        // 랜덤하게 모든 카드를 섞기
+        totalCardData.sort(() => Math.random() - 0.5);
 
-    //CardList Component 생성 과 동시에 모든 카드 렌더링 후 cardList 에 추가
-    new CardList($cardList, cardData, this.level);
+        //카드 그려주기
+        this.paintCard(totalCardData);
 
-    this.$target.appendChild($cardList);
-  }
-
-  timer(limitTime) {
-    // 시간을 흘러가게 하면서 시간이 종료되면 모달창을 띄워준 뒤 초기화면
-    // 게임을 클리어하면 타이머함수를 종료시킨뒤에 모달창을 띄우고 시간을 변수에 저장
-    // Class 프로퍼티에 저장해주는 이유 => 다른 곳에서도 타이머 함수를 종료시켜줘야 할 상황이 있을수도 있음.
-    this.timerId = setInterval(() => {
-      limitTime--;
-      if (limitTime === 0) {
-        //alert("시간종료 !");
-        console.log(this.limitTime);
-        this.modal();
-        this.defeat++;
-        this.saveScoreInLocal();
-      }
-    }, 1000);
-  }
-
-  inGameStyle() {
-    document.querySelector(".tit-game").classList.add("ir");
-    document.body.classList.add("inGame");
-  }
-
-  resetStyle() {
-    this.setup();
-    this.initialBackground();
-    clearInterval(this.timerId);
-  }
-
-  initialBackground() {
-    document.querySelector(".tit-game").classList.remove("ir");
-    document.body.classList.remove("inGame");
-  }
-
-  flipCard(event) {
-    if (!(event.target.className === "div-back")) {
-      return;
+        //타이머 실행
+        this.timer(this.limitTime);
     }
 
-    this.clickCount += 1;
+    //카드 Elements 생성해서 Rendor 하기
+    paintCard(cardData) {
+        const $cardList = document.createElement("ul");
+        $cardList.classList.add("list-card");
+        $cardList.addEventListener("click", this.flipCard.bind(this));
 
-    const $cardItem = event.target.parentNode;
-    const $back = event.target;
-    const $front = $back.nextSibling.nextSibling;
+        //CardList Component 생성 과 동시에 모든 카드 렌더링 후 cardList 에 추가
+        new CardList($cardList, cardData, this.level);
 
-    $cardItem.classList.add("active");
-    $front.classList.remove("div-hidden");
-
-    const currentCardData = $front.style.backgroundPosition.split(" ").map((item) => parseInt(item)); // 현재 클릭 카드의 [x, y] 값
-
-    if (!this.prevCardData) {
-      // 첫번째로 클릭 카드의 데이터가 없으면
-      this.prevCardData = currentCardData; // Card 데이터 넣어주고
-      this.$prevCardItem = event.target.parentNode; //Card $List 넣어줘야함
-    } else {
-      //첫번째 카드 데이터가 있으면 아래 로직
-      if (JSON.stringify(this.prevCardData) == JSON.stringify(currentCardData)) {
-        setTimeout(() => {
-          this.gotCard(event.target.parentNode);
-        }, 400);
-      } else {
-        this.failScore++;
-        this.totalFailScore++;
-      }
+        this.$target.appendChild($cardList);
     }
-    //2장 모두 뒤집었을때 초기화 코드
-    if (this.clickCount >= 2) {
-      const $backDiv = document.querySelectorAll(".div-back");
-      $backDiv.forEach((item) => {
-        item.style.pointerEvents = "none";
-      });
-      setTimeout(() => {
-        this.$prevCardItem.classList.remove("active");
-        $cardItem.classList.remove("active");
-        this.replaceCard();
-        this.prevCardData = "";
-        this.$prevCardItem = "";
-      }, 800);
-      this.clickCount = 0;
 
-      return;
+    timer(limitTime) {
+        // 시간을 흘러가게 하면서 시간이 종료되면 모달창을 띄워준 뒤 초기화면
+        // 게임을 클리어하면 타이머함수를 종료시킨뒤에 모달창을 띄우고 시간을 변수에 저장
+        // Class 프로퍼티에 저장해주는 이유 => 다른 곳에서도 타이머 함수를 종료시켜줘야 할 상황이 있을수도 있음.
+        this.timerId = setInterval(() => {
+            limitTime--;
+            // console.log(limitTime);
+            if (limitTime === 0) {
+                //alert("시간종료 !");
+                this.modal();
+                this.defeat++;
+                this.saveScoreInLocal();
+            }
+        }, 1000);
     }
-    // 카드 카운트가 2가 되면 모든 카드 다시 뒤집기
-  }
 
-  gotCard(colletCard) {
-    // 비교해서 맞는거 할라면 data-x , data-y 비교해서 같으면 Got
-    this.$prevCardItem.classList.add("div-hidden");
-    colletCard.classList.add("div-hidden");
-
-    this.hitScore++;
-    this.totalHitScore++;
-
-    this.checkHitScore(this.hitScore);
-  }
-
-  // 카드 뒤집기
-  replaceCard() {
-    const $backDiv = document.querySelectorAll(".div-back");
-    const $frontDiv = document.querySelectorAll(".div-front");
-    $frontDiv.forEach((item) => {
-      item.classList.add("div-hidden");
-    });
-    $backDiv.forEach((item) => {
-      item.classList.remove("div-hidden");
-      item.style.pointerEvents = "auto";
-    });
-  }
-
-  resetScore() {
-    this.hitScore = 0;
-    this.failScore = 0;
-  }
-
-  modal() {
-    new Modal(this.limitTime, this.hitScore, this.failScore, this.resetStyle, this.restart);
-
-    const btnGoMain = document.querySelector(".btn-go-main");
-    const btnReturn = document.querySelector(".btn-return");
-
-    btnGoMain.addEventListener("click", (event) => {
-      console.log("메인으로");
-      this.resetStyle();
-      this.resetScore();
-    });
-    btnReturn.addEventListener("click", (event) => {
-      console.log("재시도");
-      this.paintCardGame(this.level);
-      this.resetScore();
-    });
-  }
-  //4레벨 -> 8점, 6레벨 -> 18점, 8레벨 -> 32점 level**2 / 2
-  checkHitScore(score) {
-    if (score === this.level ** 2 / 2) {
-      setTimeout(() => {
-        this.modal();
-        this.victory++;
-        this.saveScoreInLocal();
-      }, 1000);
+    // 게임 실행 화면 스타일
+    inGameStyle() {
+        document.querySelector(".tit-game").classList.add("ir");
+        document.body.classList.add("inGame");
     }
-  }
 
-  saveScoreInLocal() {
-    localStorage.setItem(
-      "scoreBoard",
-      JSON.stringify({
-        victory: this.victory,
-        defeat: this.defeat,
-        totalFlipCount: this.hitScore + this.failScore,
-        totalHitScore: this.totalHitScore,
-        totalFailScore: this.totalFailScore,
-      })
-    );
-  }
+    // 메인으로 이동했을 때 메인 화면 스타일로 초기화
+    resetStyle() {
+        this.setup();
+        this.initialBackground();
+        clearInterval(this.timerId);
+    }
 
-  // 총승리, 총패배, 난이도별 클리어타임, 뒤집은 총 횟수, 맞은 횟수 , 틀린 횟수
+    // 메인 화면 스타일
+    initialBackground() {
+        document.querySelector(".tit-game").classList.remove("ir");
+        document.body.classList.remove("inGame");
+    }
+
+    // 카드 뒤집기
+    flipCard(event) {
+        if (!(event.target.className === "div-back")) {
+            return;
+        }
+
+        this.clickCount += 1;
+
+        const $cardItem = event.target.parentNode;
+        const $back = event.target;
+        const $front = $back.nextSibling.nextSibling;
+
+        $cardItem.classList.add("active");
+        $front.classList.remove("div-hidden");
+
+        const currentCardData = $front.style.backgroundPosition
+            .split(" ")
+            .map((item) => parseInt(item)); // 현재 클릭 카드의 [x, y] 값
+
+        if (!this.prevCardData) {
+            // 첫번째로 클릭 카드의 데이터가 없으면
+            this.prevCardData = currentCardData; // Card 데이터 넣어주고
+            this.$prevCardItem = event.target.parentNode; //Card $List 넣어줘야함
+        } else {
+            //첫번째 카드 데이터가 있으면 아래 로직
+            if (
+                JSON.stringify(this.prevCardData) ==
+                JSON.stringify(currentCardData)
+            ) {
+                setTimeout(() => {
+                    this.gotCard(event.target.parentNode);
+                }, 400);
+            } else {
+                this.failScore++;
+                this.totalFailScore++;
+            }
+        }
+        //2장 모두 뒤집었을때 초기화 코드
+        if (this.clickCount >= 2) {
+            const $backDiv = document.querySelectorAll(".div-back");
+            $backDiv.forEach((item) => {
+                item.style.pointerEvents = "none";
+            });
+            setTimeout(() => {
+                this.$prevCardItem.classList.remove("active");
+                $cardItem.classList.remove("active");
+                this.replaceCard();
+                this.prevCardData = "";
+                this.$prevCardItem = "";
+            }, 800);
+            this.clickCount = 0;
+
+            return;
+        }
+        // 카드 카운트가 2가 되면 모든 카드 다시 뒤집기
+    }
+
+    // 동일한 카드를 찾았을 경우
+    gotCard(colletCard) {
+        // 비교해서 맞는거 할라면 data-x , data-y 비교해서 같으면 Got
+        this.$prevCardItem.classList.add("div-hidden");
+        colletCard.classList.add("div-hidden");
+
+        this.hitScore++;
+        this.totalHitScore++;
+
+        this.checkHitScore(this.hitScore);
+    }
+
+    // 선택했던 카드 다시 뒤집기
+    replaceCard() {
+        const $backDiv = document.querySelectorAll(".div-back");
+        const $frontDiv = document.querySelectorAll(".div-front");
+        $frontDiv.forEach((item) => {
+            item.classList.add("div-hidden");
+        });
+        $backDiv.forEach((item) => {
+            item.classList.remove("div-hidden");
+            item.style.pointerEvents = "auto";
+        });
+    }
+
+    // 한 게임에 대한 모달창 결과를 위한 스코어 리셋
+    resetScore() {
+        this.hitScore = 0;
+        this.failScore = 0;
+    }
+
+    // 타이머 시간 안에 카드를 모두 뒤집었을 때
+    //4레벨 -> 8점, 6레벨 -> 18점, 8레벨 -> 32점 level**2 / 2
+    checkHitScore(score) {
+        if (score === this.level ** 2 / 2) {
+            setTimeout(() => {
+                this.modal();
+                this.victory++;
+                this.saveScoreInLocal();
+            }, 1000);
+        }
+    }
+
+    modal() {
+        new Modal(this.limitTime, this.hitScore, this.failScore);
+
+        const btnGoMain = document.querySelector(".btn-go-main");
+        const btnReturn = document.querySelector(".btn-return");
+
+        btnGoMain.addEventListener("click", (event) => {
+            console.log("메인으로");
+            this.resetStyle();
+            this.resetScore();
+        });
+        btnReturn.addEventListener("click", (event) => {
+            console.log("재시도");
+            this.paintCardGame(this.level);
+            this.resetScore();
+        });
+    }
+
+    // 로컬스토리지에 스코어 정보 저장하기
+    saveScoreInLocal() {
+        localStorage.setItem(
+            "scoreBoard",
+            JSON.stringify({
+                victory: this.victory,
+                defeat: this.defeat,
+                // totalFlipCount: this.totalHitScore + this.totalFailScore,
+                totalHitScore: this.totalHitScore,
+                totalFailScore: this.totalFailScore,
+            })
+        );
+    }
 }
 export default CardGame;
