e985e1c31c53d292244713794080d3a8ab6514db
tnghgks <tnghgks@naver.com>
2023-04-09 18:11:04 +0900

    style: console.log 삭제

--- js/api/client.js
+++ js/api/client.js
@@ -4,7 +4,7 @@ export const request = async (url, options = {}) => {
 
     if (response.ok) {
       const data = await response.json();
-      console.log(data);
+
       return data;
     }
     throw new Error("API 통신 실패");
